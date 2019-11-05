900b918a8ae949c1bd136e62a9b5ac4aa273dbca
Thy Nguyen <nguyenthy2408@gmail.com>
2019-11-05 16:57:10 +0700

    Initial Commit

---
+++ MyMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMovie
+//
+//  Created by Tran Lan on 11/5/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
